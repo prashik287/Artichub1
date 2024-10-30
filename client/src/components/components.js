699e339d142776f167178fb6c789f43be61c268f
Prashik Jadhav <73395973+prashik287@users.noreply.github.com>
2024-10-30 17:14:48 +0530

    Add files via upload

---
+++ client/src/components/components.js
@@ -0,0 +1,27 @@
+import Navbar from "./header/Navbar";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import VerificationSuccess from "./pages/verification/VerificationSuccess";
+import VerificationFail from "./pages/verification/VerificationFail";
+import Sidebar from "./sidebar/Sidebar";
+import Dashboard from './pages/Dashboard'
+import Contact from "./pages/Contact";
+import CustomerCareForm from './pages/CustomerCareForm'
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import AdminLogin from "./pages/AdminLogin";
+import ManageUsers from "./Admin/ManageUsers";
+import ProductDetails from "./pages/ProductDetails"
+import ResendVerification from "./pages/verification/ResendVerification";
+import AccountSettings from './pages/user/AccountSettings'
+import BuyService from './pages/Buy.jsx';
+import Checkout from "./pages/Checkout.jsx";
+import Preferences from './pages/user/Preferences.jsx'
+import MyOrder from './pages/user/MyOrder.jsx'
+import AddProduct from "./pages/user/AddProduct.jsx";
+import Auction from "./pages/Auction.jsx";
+import ProductBid from "./pages/ProductBid.jsx";
+import ListingManager from "./pages/ListingManager.jsx";
+import PasswordResetRequest from "./pages/PasswordResetRequest.jsx";
+export { Navbar, Login, Signup, Home, VerificationSuccess, VerificationFail ,Sidebar , Dashboard , Contact , CustomerCareForm,PrivacyPolicy,AdminLogin,ManageUsers,ProductDetails,ResendVerification,AccountSettings,BuyService,Checkout,Preferences,MyOrder,AddProduct,Auction,ProductBid,ListingManager,PasswordResetRequest};
+
